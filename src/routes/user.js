e862a2b982534a5311141ea10532625b8aa3b0ab
githubnali <nagarajunnr341@gmail.com>
2025-08-12 21:29:24 +0530

    [user connection either fromUser or toUSer /user/connections]

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const ConnectionRequest = require('../models/connectionRequest');
 
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills"
+
 //get all the pending connection requests from logged in user
 userRouter.get("/user/requests/received", userAuth, async (req, resp) => {
     try {
@@ -12,7 +14,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, resp) => {
         const connectionRequest = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        }).populate("fromUserId", "firstName lastName photoUrl age gender about skills")
+        }).populate("fromUserId", USER_SAFE_DATA)
 
         resp.json({
             message: "data fetched successfully",
@@ -20,6 +22,34 @@ userRouter.get("/user/requests/received", userAuth, async (req, resp) => {
         })
     }catch(err) {
         resp.statusCode(400).send("ERROR: " + err.message)
+    }
+})
+
+//check the connection of the user
+userRouter.get("/user/connections", userAuth, async(req, resp) => {
+    try{
+        const loggedInUser = req.user;
+
+        //find the user connection either fromUser/toUser
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id, status: "accepted"},
+                {fromUserId: loggedInUser._id, status:"accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequest.map((row) =>  {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+               return row.toUserId
+            }
+            return row.fromUserId
+        });
+
+        resp.json({
+            data
+        })
+    }catch(err) {
+        resp.status(400).send({message: err.message})
     }
 })
 
